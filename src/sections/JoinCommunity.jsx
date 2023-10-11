a93c0868700228bfac6c0751c4f31734315849ef
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-11 12:46:10 +0530

    new

--- src/sections/JoinCommunity.jsx
+++ src/sections/JoinCommunity.jsx
@@ -26,50 +26,40 @@ const SocialLink = ({ children }) => {
 function JoinCommunity() {
   return (
     <section className="pb-14 relative z-10 overflow-hidden">
-  
-    
-
       <div className="container-wrapper">
-        <div className=" flex items-center justify-between">
-       
+        <div className=" flex items-center justify-between"></div>
 
-       
-        </div>
-
-        <div className="flex flex-col lg:flex-row items-center justify-between mb-2 lg:mb-20 text-center lg:text-left space-y-6 lg:space-y-0">
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold font-dream lh-1_4">
-            JOIN OUR <br />
-            COMMUNITY
-          </h1>
-
-          <div className="max-w-[20rem] w-full space-y-8">
-            
-            <div className="flex items-center -m-1 flex-wrap justify-center lg:justify-normal">
-            <Button className="m-1">Tokenomics</Button>
-            <Button link="https://twitter.com/barbielovepepe" className="m-1">
-              Twitter
-            </Button>
-            <Button link="https://t.me/barbielovepepe" className="m-1">
-              Telegram
-            </Button>
-          
-          </div>
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-10 mb-2 lg:mb-20 text-center lg:text-left space-y-6 lg:space-y-0">
+          <img src="images/foot-logo.png" alt="" className="h-[14rem]" />
+          <div className=" w-full space-y-8">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold font-dream lh-1_4">
+              JOIN OUR COMMUNITY
+            </h1>
+           
 
             <input
               type="text"
               className="flex-1 h-14 bg-white rounded-full px-6 text-black w-full text-base"
               placeholder="Your email"
             />
+
+<div className="flex items-center -m-1 flex-wrap justify-center lg:justify-normal">
+              <Button className="m-1">Tokenomics</Button>
+              <Button link="https://twitter.com/barbielovepepe" className="m-1">
+                Twitter
+              </Button>
+              <Button link="https://t.me/barbielovepepe" className="m-1">
+                Telegram
+              </Button>
+            </div>
           </div>
         </div>
 
         <div className="flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0">
-     
           <p className="text-center lg:text-left">
             All rights reserved by BarbieLovePepe 2023
           </p>
         </div>
-     
       </div>
     </section>
   );
