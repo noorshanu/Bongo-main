ef5bd6163fe25bee113c27f6e7d3e8d3ca9e74c8
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-11 13:51:48 +0530

    Update Product.jsx

--- src/sections/Product.jsx
+++ src/sections/Product.jsx
@@ -9,7 +9,11 @@ function Product() {
           <Title className="text-black">Bongo cat features</Title>
         </div>
 
-        <div className="flex flex-col sm:flex-row justify-between gap-3 my-12">
+        <div className="flex flex-col sm:flex-row justify-between gap-3 my-12 relative z-10">
+          <div>
+            <img src="images/cream.png" alt="" className=" -top-[120%] absolute -left-[40%] -z-10 w-auto h-[800px]" />
+          </div>
+
           <img src="images/1.png" alt=""  className="h-[200px]" />
           <img src="images/2.png" alt="" className="h-[200px]" />
           <img src="images/3.png" alt=""  className="h-[200px]"/>
