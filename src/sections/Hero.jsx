8b9328bbbb8c0e538fe510b40bd4a1cc5a5eae7c
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-11 18:42:27 +0530

    new

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,9 +8,11 @@ function Hero() {
       />
 
       <div className="container-wrapper lg:h-screen flex flex-col items-center justify-center pt-6 pb-12 lg:pt-0 lg:pb-0 lg:drop-shadow-[0_0_10rem_white]">
-        <img src="images/cloud1.png" alt="" className="x1 absolute top-[5%] sm:top-[20%] left-0 w-auto h-[50px] sm:h-[120px] z-50" />
+        <img src="images/cloud1.png" alt="" className="x1 absolute top-[5%] sm:top-[20%] left-0 w-auto h-[50px] sm:h-[160px]  " />
         <img src="images/star.png" alt="" className=" absolute top-[46%] left-[10%] w-auto h-[120px] hidden sm:block" />
-        <img src="images/cloud2.png" alt="" className=" absolute top-[40%] right-0 w-auto h-[120px] hidden sm:block" />
+        <img src="images/cloud2.png" alt="" className="x2 absolute w-auto h-[170px] hidden sm:block" />
+        <img src="images/cloud2.png" alt="" className="x4 absolute w-auto h-[150px] hidden sm:block" />
+        <img src="images/cloud1.png" alt="" className="x3 absolute top-[5%] sm:top-[20%] left-0 w-auto h-[50px] sm:h-[170px]  " />
         <img
           src="/images/hero-img.png"
           className="w-full lg:w-[55%] lg:h-[60%] object-contain"
